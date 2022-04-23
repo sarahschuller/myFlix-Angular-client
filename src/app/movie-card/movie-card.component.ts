c0a6f6f69e41763c3d4c478e45375238ea9bb4b2
sarahschuller <93050611+sarahschuller@users.noreply.github.com>
2022-04-23 11:59:07 -0400

    add code for movie-card component

--- src/app/movie-card/movie-card.component.ts
+++ src/app/movie-card/movie-card.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, } from '@angular/core';
+import { FetchApiDataService } from '../fetch-api-data.service'
 
 @Component({
   selector: 'app-movie-card',
   templateUrl: './movie-card.component.html',
   styleUrls: ['./movie-card.component.scss']
 })
-export class MovieCardComponent implements OnInit {
+export class MovieCardComponent {
+  movies: any[] = [];
+  constructor(public fetchApiData: FetchApiDataService) { }
 
-  constructor() { }
+ngOnInit(): void {
+  this.getMovies();
+}
 
-  ngOnInit(): void {
+getMovies(): void {
+  this.fetchApiData.getAllMovies().subscribe((resp: any) => {
+      this.movies = resp;
+      console.log(this.movies);
+      return this.movies;
+    });
   }
-
 }
