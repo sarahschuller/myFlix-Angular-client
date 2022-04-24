f7267921b25a92791d6b93909259ffc601ccdd84
sarahschuller <93050611+sarahschuller@users.noreply.github.com>
2022-04-24 14:41:34 -0400

    update navigation component

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FetchApiDataService } from '../fetch-api-data.service';
+import { Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatDialogModule } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-navigation',
@@ -7,9 +11,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavigationComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public fetchApiData: FetchApiDataService,
+    public router: Router,
+    public snackBar: MatSnackBar,
+    public dialog: MatDialogModule
+  ) { }
 
   ngOnInit(): void {
   }
-
+  toMovies(): void {
+    this.router.navigate(['movies']);
+  }
+  goToProfilePage(): void {
+    this.router.navigate(['profile']);
+  }
+  logOut(): void {
+    localStorage.clear();
+    this.snackBar.open('You have been successfully logged out', 'Ok', {
+      duration: 2000,
+    });
+    this.router.navigate(['welcome']);
+  }
 }
