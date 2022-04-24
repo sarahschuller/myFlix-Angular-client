3ed559c93522166262e74a5da2662e7ac9a2d2b5
sarahschuller <93050611+sarahschuller@users.noreply.github.com>
2022-04-24 14:39:42 -0400

    update user profile

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { FetchApiDataService } from '../fetch-api-data.service';
+
+// Component Imports
+import { DirectorCardComponent } from '../director-card/director-card.component';
+import { GenreCardComponent } from '../genre-card/genre-card.component';
+import { MovieDescriptionComponent } from '../movie-description/movie-description.component';
+import { EditProfileComponent } from '../edit-profile/edit-profile.component';
 
 @Component({
   selector: 'app-user-profile',
@@ -6,10 +16,103 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-profile.component.scss']
 })
 export class UserProfileComponent implements OnInit {
+  user: any = {};
+  movies: any[] = [];
+  username: any = localStorage.getItem('user');
+  favorites: any = null;
+  favoriteMovies: any[] = [];
+  displayElement: boolean = false
 
-  constructor() { }
+  constructor(
+    public fetchApiData: FetchApiDataService,
+    public snackBar: MatSnackBar,
+    public dialog: MatDialog,
+    public router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.getUserProfile();
+    this.getFavoriteMovies()  
   }
 
+  openMovieDescription(title: string, imagePath: any, description: string): void {
+    this.dialog.open(MovieDescriptionComponent, {
+      data: {
+        Title: title,
+        ImagePath: imagePath,
+        Description: description,
+      },
+      width: '500px'
+    });
+   
+  }
+  openDirectorCard(name: string, bio: string, birth: string): void {
+    this.dialog.open(DirectorCardComponent, {
+      data: {Name: name, Bio: bio, Birth: birth},
+      width: '500px',
+    });
+  }
+  // Open Genre View
+  openGenreCard(name: string, description: string): void {
+    this.dialog.open(GenreCardComponent, {
+      data: {
+        Name: name,
+        Description: description,
+      },
+      width: '500px'
+    });
+  }
+
+  getUserProfile(): void {
+    const user = localStorage.getItem('user');
+    if (user) {
+      this.fetchApiData.getUserProfile(user).subscribe((resp: any) => {
+        this.user = resp;
+        console.log(this.user);
+      });
+    }
+  }
+  
+  editUserProfile(): void {
+    this.dialog.open(EditProfileComponent, {
+      width: '500px'
+    });
+  }
+  
+  getFavoriteMovies(): void {
+    let movies: any[] = [];
+    this.fetchApiData.getAllMovies().subscribe((res: any) => {
+      movies = res;
+      movies.forEach((movie: any) => {
+        if (this.user.FavouriteMovies.includes(movie._id)) {
+          this.favoriteMovies.push(movie);
+          this.displayElement = true;
+        }
+        });
+    });
+  }
+        
+  //Delete User account
+  deleteUserProfile(): void {
+    if (confirm('Are you sure? This cannot be undone.')) {
+      this.router.navigate(['welcome']).then(() => {
+        this.snackBar.open('Your account was deleted', 'OK', {duration: 6000});
+      });
+      this.router.navigate(['welcome'])
+      this.fetchApiData.deleteUserProfile().subscribe(() => {
+        localStorage.clear();
+      });
+    }
+  
+  }
+  // Delete Favorite Movies
+  deleteFavoriteMovies(id: string): void {
+    this.fetchApiData.deleteFavoriteMovies(id).subscribe((res: any) => {
+      this.snackBar.open('Successfully removed from favorite movies.', 'OK', {
+        duration: 2000,
+      });
+      this.ngOnInit();
+      return this.favorites;
+    })
+  }
 }
